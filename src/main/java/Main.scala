9b9117e80e649cc2032364d975e47c4b9ab7e82f
moha <mohcine.sirah@gmail.com>
2019-10-12 17:47:57 +0200

    modif with conflict

--- src/main/java/Main.scala
+++ src/main/java/Main.scala
@@ -1,6 +1,6 @@
 object Main extends  App {
 
-  println("bismi lah , c'est first modif + la dev maintenant")
+  println("bismi lah , c'est first modif + la dev maintenant + 2eme modif")
   println("c'est la dev + first Modif maintenant")
   println("c'est la é eme modif")
 
