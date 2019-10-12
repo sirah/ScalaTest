974fb212f5518af530eca81154d40e8a0c6551a2
moha <mohcine.sirah@gmail.com>
2019-10-12 18:12:03 +0200

    nadaaaé

--- src/main/java/Main.scala
+++ src/main/java/Main.scala
@@ -1,9 +1,3 @@
 object Main extends  App {
-  println("secondModif")
-  println("bismi lah , je veux le merge")
-  println("c'est le merge que je cherche")
-  println("c'est la é eme modif")
-
-
 
 }
